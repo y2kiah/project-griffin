6ed26ad4158469becf22527f75274af1b5bb8a7c
Jeff Kiah <y2kiah@hotmail.com>
2015-07-19 12:36:16 -0400

    started adding when_all and when_any

--- project-griffin/project-griffin/source/utility/concurrency.h
+++ project-griffin/project-griffin/source/utility/concurrency.h
@@ -25,167 +25,11 @@
 
 namespace griffin {
 
-	/**
-	* @class concurrent
-	* The concurrent class wraps any class T and accepts lambdas that take a reference to the contained
-	* object for performing operations (via member-function calls presumably) in a thread safe manner.
-	* Lambdas are queued in a concurrent_queue and run by a private worker thread. This is the non-
-	* blocking asychronous way to wrap a class for thread safety, and is highly composable with async
-	* patterns. Since each instance of this object creates its own thread, don't abuse this pattern by
-	* creating lots of these objects. This should wrap a high level class with few instances.
-	* See the backgrounder class example below for an illustration.
-	* @tparam	T	type of class or data wrapped for concurrency
-	*/
-	template <typename T>
-	class concurrent {
-	public:
-		concurrent(T t_ = T{}) :
-			t(std::move(t_)),
-			worker{ [=]{ while (!done) { q.wait_pop()(); } } }
-		{}
-
-		~concurrent() {
-			q.push([=]{ done = true; });
-			worker.join();
-		}
-
-		/**
-		* Executes function pointer, functor or lambda on the worker thread where the task is
-		* internally serialized by the concurrent queue for thread safety.
-		* @tparam	F	function pointer, functor or lambda accepting one argument of type T
-		* @param	f	type F
-		* @returns std::future of type T
-		*/
-		template <typename F>
-		auto operator()(F f) const -> std::future<decltype(f(t))> {
-			auto p = std::make_shared<std::promise<decltype(f(t))>>();
-			auto ret = p->get_future();
-
-			q.push([=]{
-				try {
-					set_value(*p, f, t);
-				}
-				catch (...) {
-					p->set_exception(std::current_exception());
-				}
-			});
-
-			return ret;
-		}
-
-	private:
-		mutable T t;
-		mutable concurrent_queue<std::function<void()>> q;
-		bool done = false;
-		std::thread worker;
-
-		/**
-		* Helpers for p.set_value() so the function above compiles for both void and non-void.
-		*/
-		template <typename Fut, typename F, typename T>
-		static void set_value(std::promise<Fut>& p, F& f, T& t) {
-			p.set_value(f(t));
-		}
-
-		template <typename F, typename T>
-		static void set_value(std::promise<void>& p, F& f, T& t) {
-			f(t);
-			p.set_value();
-		}
-	};
-
-
-	/**
-	* @class monitor
-	* The monitor class wraps any class T and accepts lambdas that take a reference to the
-	* contained object for performing operations (via member-function calls presumably) in a thread
-	* safe manner. Invoked lambdas are internally synchronized using a mutex, so this would
-	* potentially block client threads and cause contention. This should almost never be used,
-	* prefer the concurrent<T> class and task parallelism when possible.
-	* @tparam	T	class or data wrapped to be protected by a mutex for thread safety
-	*/
-	template <typename T>
-	class monitor {
-	public:
-		monitor(T t_ = T{}) : t(t_) {}
-
-		/*template <typename F>
-		auto operator()(F f) const -> decltype(f(t)) {
-			std::lock_guard<std::mutex> _{ m };
-			return f(t);
-		}*/
-
-		template <typename F>
-		auto operator()(F f) const -> typename std::result_of<F(T)>::type {
-			std::lock_guard<std::mutex> _{ m };
-			return f(t);
-		}
-
-	private:
-		mutable T t;
-		mutable std::mutex m;
-	};
-
-
-	/**
-	* Example usage of concurrent<T>.
-	* @code
-	*	concurrent<string> cs;
-	*	auto f = cs([](string& s) {		// C++14 polymorphic lambda becomes cs([](s) {
-	*		// thread safe transaction!
-	*		s += "added thread-safe";	// this is operating on the internal (wrapped) object
-	*		return string("this was ") + s;
-	*	});
-	*	// f is a std::future for the lambda return value
-	* @endcode
-	*/
-
-	/**
-	* Example usage of concurrent<T>.
-	* One way to use the concurrent<t> class is to protect a private data member of an outer class
-	* (call it backgrounder for example) that exposes a simple public API. That way the weird
-	* usage pattern of submitting lambdas can be hidden within the implementation of the public
-	* APIs, and client code will not have to know or care about the concurrent<t> usage pattern.
-	* @code
-	*	class backgrounder {
-	*	public:
-	*		std::future<bool> save(std::string file) {
-	*			c([=](data& d) {
-	*				// each function is an ordered transaction
-	*			});
-	*		}
-	*
-	*		std::future<size_t> print(some& stuff) {
-	*			c([=, &stuff](data& d) {
-	*				// atomic and indivisible w.r.t. other functions
-	*			});
-	*		}
-	*
-	*	private:
-	*		struct data {  };	// private data is unshared by construction
-	*		concurrent<data> c;	// fully thread-safe internal data
-	*	};
-	* @endcode
-	*/
-
-	/**
-	* Example usage of monitor<T>.
-	* @code
-	*	monitor<string> s;
-	*	auto f = s([](string& s) {		// C++14 polymorphic lambda becomes s([](s) {
-	*		// thread safe transaction!
-	*		s += "added thread-safe";	// this is operating on the internal (wrapped) object
-	*		return string("this was ") + s;
-	*	});
-	*	// f is a string (note that with concurrent<t> this would be a future)
-	* @endcode
-	*/
-
-
-	// The following classes may be built for cross platform compiling based on use of PPL
-#define MAX_WORKER_THREADS	8
-#define NUM_FIXED_THREADS	3
-#define NUM_TASK_QUEUES		NUM_FIXED_THREADS + 1	// one shared by all worker threads, plus one each for fixed thread affinity
+#define CONCURRENT_MAX_WORKER_THREADS	8
+#define CONCURRENT_NUM_FIXED_THREADS	3
+// one shared by all worker threads, plus one each for fixed thread affinity
+#define CONCURRENT_NUM_TASK_QUEUES		CONCURRENT_NUM_FIXED_THREADS + 1
+	
 	enum ThreadAffinity : uint8_t {
 		Thread_Workers = 0,
 		Thread_OS_Input,
@@ -195,14 +39,14 @@ namespace griffin {
 
 	class thread_pool {
 	public:
-		typedef std::array<std::thread, MAX_WORKER_THREADS> ThreadList;
-		typedef std::array<concurrent_queue<std::function<void()>>, NUM_TASK_QUEUES> TaskQueueList;
+		typedef std::array<std::thread, CONCURRENT_MAX_WORKER_THREADS> ThreadList;
+		typedef std::array<concurrent_queue<std::function<void()>>, CONCURRENT_NUM_TASK_QUEUES> TaskQueueList;
 
 		explicit thread_pool(int cpuCount) {
 			//assert(m_busy.is_lock_free());
 
 			// start up one worker thread per core
-			m_numWorkerThreads = cpuCount > MAX_WORKER_THREADS ? MAX_WORKER_THREADS : cpuCount;
+			m_numWorkerThreads = cpuCount > CONCURRENT_MAX_WORKER_THREADS ? CONCURRENT_MAX_WORKER_THREADS : cpuCount;
 			
 			auto threadProcess = [=]{
 				while (!m_done) { m_tasks[Thread_Workers].wait_pop()(); }
@@ -277,9 +121,7 @@ namespace griffin {
 
 		// Functions
 
-		//template <typename F>
-		task(/*F f_, */ThreadAffinity threadAffinity_ = Thread_Workers) :
-			//f{ f_ },
+		task(ThreadAffinity threadAffinity_ = Thread_Workers) :
 			_pImpl(std::make_shared<Impl>())
 		{
 			_pImpl->result = _pImpl->p.get_future();
@@ -387,6 +229,26 @@ namespace griffin {
 		}
 	};
 
+	template<typename F, typename... Tasks>
+	task<void> when_all(F&& f, Tasks&...) {
+		task<void> ret;
+		ret.run([]{
+			return f();
+		});
+
+		return ret;
+	}
+
+	template<typename F, typename... Tasks>
+	task<void> when_any(F&& f, Tasks&...) {
+		task<void> ret;
+		ret.run([]{
+			return f();
+		});
+
+		return ret;
+	}
+
 	/*
 	template <typename T>
 	class task_group {
@@ -406,5 +268,161 @@ namespace griffin {
 		vector<packaged_task<T>> m_tasks;
 	};
 */
+
+	/**
+	* @class concurrent
+	* The concurrent class wraps any class T and accepts lambdas that take a reference to the contained
+	* object for performing operations (via member-function calls presumably) in a thread safe manner.
+	* Lambdas are queued in a concurrent_queue and run by a private worker thread. This is the non-
+	* blocking asychronous way to wrap a class for thread safety, and is highly composable with async
+	* patterns. Since each instance of this object creates its own thread, don't abuse this pattern by
+	* creating lots of these objects. This should wrap a high level class with few instances.
+	* See the backgrounder class example below for an illustration.
+	* @tparam	T	type of class or data wrapped for concurrency
+	*/
+	template <typename T>
+	class concurrent {
+	public:
+		concurrent(T t_ = T{}) :
+			t(std::move(t_)),
+			worker{ [=]{ while (!done) { q.wait_pop()(); } } }
+		{}
+
+		~concurrent() {
+			q.push([=]{ done = true; });
+			worker.join();
+		}
+
+		/**
+		* Executes function pointer, functor or lambda on the worker thread where the task is
+		* internally serialized by the concurrent queue for thread safety.
+		* @tparam	F	function pointer, functor or lambda accepting one argument of type T
+		* @param	f	type F
+		* @returns std::future of type T
+		*/
+		template <typename F>
+		auto operator()(F f) const -> std::future<decltype(f(t))> {
+			auto p = std::make_shared<std::promise<decltype(f(t))>>();
+			auto ret = p->get_future();
+
+			q.push([=]{
+				try {
+					set_value(*p, f, t);
+				}
+				catch (...) {
+					p->set_exception(std::current_exception());
+				}
+			});
+
+			return ret;
+		}
+
+	private:
+		mutable T t;
+		mutable concurrent_queue<std::function<void()>> q;
+		bool done = false;
+		std::thread worker;
+
+		/**
+		* Helpers for p.set_value() so the function above compiles for both void and non-void.
+		*/
+		template <typename Fut, typename F, typename T>
+		static void set_value(std::promise<Fut>& p, F& f, T& t) {
+			p.set_value(f(t));
+		}
+
+		template <typename F, typename T>
+		static void set_value(std::promise<void>& p, F& f, T& t) {
+			f(t);
+			p.set_value();
+		}
+	};
+
+
+	/**
+	* @class monitor
+	* The monitor class wraps any class T and accepts lambdas that take a reference to the
+	* contained object for performing operations (via member-function calls presumably) in a thread
+	* safe manner. Invoked lambdas are internally synchronized using a mutex, so this would
+	* potentially block client threads and cause contention. This should almost never be used,
+	* prefer the concurrent<T> class and task parallelism when possible.
+	* @tparam	T	class or data wrapped to be protected by a mutex for thread safety
+	*/
+	template <typename T>
+	class monitor {
+	public:
+		monitor(T t_ = T{}) : t(t_) {}
+
+		/*template <typename F>
+		auto operator()(F f) const -> decltype(f(t)) {
+		std::lock_guard<std::mutex> _{ m };
+		return f(t);
+		}*/
+
+		template <typename F>
+		auto operator()(F f) const -> typename std::result_of<F(T)>::type {
+			std::lock_guard<std::mutex> _{ m };
+			return f(t);
+		}
+
+	private:
+		mutable T t;
+		mutable std::mutex m;
+	};
+
+
+	/**
+	* Example usage of concurrent<T>.
+	* @code
+	*	concurrent<string> cs;
+	*	auto f = cs([](string& s) {		// C++14 polymorphic lambda becomes cs([](s) {
+	*		// thread safe transaction!
+	*		s += "added thread-safe";	// this is operating on the internal (wrapped) object
+	*		return string("this was ") + s;
+	*	});
+	*	// f is a std::future for the lambda return value
+	* @endcode
+	*/
+
+	/**
+	* Example usage of concurrent<T>.
+	* One way to use the concurrent<t> class is to protect a private data member of an outer class
+	* (call it backgrounder for example) that exposes a simple public API. That way the weird
+	* usage pattern of submitting lambdas can be hidden within the implementation of the public
+	* APIs, and client code will not have to know or care about the concurrent<t> usage pattern.
+	* @code
+	*	class backgrounder {
+	*	public:
+	*		std::future<bool> save(std::string file) {
+	*			c([=](data& d) {
+	*				// each function is an ordered transaction
+	*			});
+	*		}
+	*
+	*		std::future<size_t> print(some& stuff) {
+	*			c([=, &stuff](data& d) {
+	*				// atomic and indivisible w.r.t. other functions
+	*			});
+	*		}
+	*
+	*	private:
+	*		struct data {  };	// private data is unshared by construction
+	*		concurrent<data> c;	// fully thread-safe internal data
+	*	};
+	* @endcode
+	*/
+
+	/**
+	* Example usage of monitor<T>.
+	* @code
+	*	monitor<string> s;
+	*	auto f = s([](string& s) {		// C++14 polymorphic lambda becomes s([](s) {
+	*		// thread safe transaction!
+	*		s += "added thread-safe";	// this is operating on the internal (wrapped) object
+	*		return string("this was ") + s;
+	*	});
+	*	// f is a string (note that with concurrent<t> this would be a future)
+	* @endcode
+	*/
 }
 #endif
