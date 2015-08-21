f86b3b1fc1ff7c88ed80e393086c8b57452735e2
Jeff Kiah <y2kiah@hotmail.com>
2015-08-21 09:56:38 -0400

    adding blend.h for tweening functions and macros

---
+++ project-griffin/project-griffin/source/utility/blend.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef GRIFFIN_BLEND_H_
+#define GRIFFIN_BLEND_H_
+
+namespace griffin {
+
+	inline float lerp(float a, float b, float t)
+	{
+		return (1.0f - t) * a + t * b;
+	}
+
+	// TODO: s-curve function
+
+	inline float quint(float a, float b, float t)
+	{
+		
+	}
+
+	// http://developer.amd.com/wordpress/media/2012/10/Andersson-TerrainRendering(Siggraph07).pdf
+	inline float overlayBlend(float base, float value, float opacity)
+	{
+		float a = (base < 0.5f) ? (2.0f * base * value) : (1.0f - 2.0f * (1.0f - base) * (1.0f - value));
+		return lerp(base, a, opacity);
+	}
+
+}
+
+#endif
